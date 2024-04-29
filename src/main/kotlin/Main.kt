2dc46aea55d88b4747e2b374f418614a9c88a8ed
Galbavý Gabriel <gabriel.galbavy@student.ssnd.sk>
2024-04-29 15:25:41 +0200

    CSV reader

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,31 @@
+import java.io.BufferedReader
+import java.io.File
+import java.io.FileReader
+
+fun parseCSV(filePath: String): List<List<String>> {
+    val data: MutableList<List<String>> = mutableListOf()
+
+    try {
+        val file = File(filePath)
+        val bufferedReader = BufferedReader(FileReader(file))
+
+        var line: String?
+        while (bufferedReader.readLine().also { line = it } != null) {
+            val values = line!!.split(",") // assuming CSV file uses comma as delimiter
+            data.add(values)
+        }
+
+        bufferedReader.close()
+    } catch (e: Exception) {
+        e.printStackTrace()
+    }
+
+    return data
+}
 fun main(args: Array<String>)
 {
+    val path = "strava.csv"
 
+    val x = parseCSV(path)
+    println(x[0][10])
 }
