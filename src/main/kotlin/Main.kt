0024c9bdb14e0c6ed05c97c9ba93cce51851045f
FilipGregus2005 <filip.gregus2005@gmail.com>
2024-04-29 17:07:38 +0200

    Print out authors

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,30 +2,41 @@ import java.io.BufferedReader
 import java.io.File
 import java.io.FileReader
 
-fun parseCSV(filePath: String): List<List<String>> {
+fun parseCSV(filePath: String): List<List<String>>
+{
     val data: MutableList<List<String>> = mutableListOf()
 
-    try {
+    try
+    {
         val file = File(filePath)
         val bufferedReader = BufferedReader(FileReader(file))
 
         var line: String?
-        while (bufferedReader.readLine().also { line = it } != null) {
+        while (bufferedReader.readLine().also { line = it } != null)
+        {
             val values = line!!.split(",") // assuming CSV file uses comma as delimiter
             data.add(values)
         }
 
         bufferedReader.close()
-    } catch (e: Exception) {
+    } catch (e: Exception)
+    {
         e.printStackTrace()
     }
 
     return data
 }
+
+fun writeNames(): Unit
+{
+    println("Filip Gregus")
+    println("Gabriel Galbavy")
+}
+
 fun main(args: Array<String>)
 {
     val path = "strava.csv"
 
     val x = parseCSV(path)
-    println(x[0][10])
+    writeNames()
 }
