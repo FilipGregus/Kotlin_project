e7fac64e37ab7742deffc6c51f1b0a323b433d0f
Galbavý Gabriel <gabriel.galbavy@student.ssnd.sk>
2024-04-29 17:56:53 +0200

    Strava file parsing

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,9 +1,9 @@
 import java.io.BufferedReader
 import java.io.File
 import java.io.FileReader
-
 fun parseCSV(filePath: String, drop: Int, trim:Boolean): List<List<String>> {
     val data: MutableList<List<String>> = mutableListOf()
+    var counter:Int = 0
 
     try {
         val file = File(filePath)
@@ -19,10 +19,11 @@ fun parseCSV(filePath: String, drop: Int, trim:Boolean): List<List<String>> {
                 if ((trim && line!!.isNotEmpty()) || !trim) {
                     val values = line!!.split(",") // assuming CSV file uses comma as delimiter
                     data.add(values)
+                    counter++
                 }
             }
         }
-
+        println("$counter lines loaded..")
         bufferedReader.close()
     } catch (e: Exception) {
         e.printStackTrace()
@@ -40,9 +41,9 @@ fun writeNames(): Unit
 fun main(args: Array<String>)
 {
     val path = "strava.csv"
-    val drop: Int = 4
+    val drop: Int = 1
 
-    val x = parseCSV(path, drop, true)
+    val x = parseCSV(path, drop, false)
     x.forEach{ println(it)}
     writeNames()
 }
